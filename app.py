0759e53be2c4074f0c98f4d3debd1ee34c184f7e
Muhammad Rafay <muhammadrafay7655@gmail.com>
2023-03-23 23:48:13 +0500

    files changes

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Counter, Gauge, Histogram, Summary, generate_latest, CONTENT_TYPE_LATEST
+from flask import Response
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
 
-@app.route('/')
-def my_api():
-    value = 0
-    # your logic to update value goes here
-    return f'The current value is {value}'
+REQUEST_COUNT = Counter('flask_app_request_count', 'Flask App Request Count', ['method', 'endpoint', 'http_status'])
+REQUEST_LATENCY = Histogram('flask_app_request_latency_seconds', 'Flask App Request Latency', ['method', 'endpoint'])
+REQUEST_GAUGE = Gauge('flask_app_request_gauge', 'Flask App Request Gauge', ['method', 'endpoint'])
+
+@app.route("/")
+def hello():
+    REQUEST_COUNT.labels(method='GET', endpoint='/').inc()
+    with REQUEST_LATENCY.labels(method='GET', endpoint='/').time():
+        # Your code here
+        time.sleep(1)
+    REQUEST_GAUGE.labels(method='GET', endpoint='/').set(42)
+    return "Hello World!"
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), content_type=CONTENT_TYPE_LATEST)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8080)
